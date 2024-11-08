f715825ac05d2a9884d9459a0938e71f486dcb4c
Ada Lundhe <ada@datavant.com>
2024-11-08 09:54:11 -0600

    AL: add error supression for response

--- werkflow-http/werkflow_http/connections/http/models/http/http_response.py
+++ werkflow-http/werkflow_http/connections/http/models/http/http_response.py
@@ -3,7 +3,7 @@ import re
 from typing import Dict, Literal, Optional, Type, TypeVar, Union
 
 import orjson
-from pydantic import BaseModel, StrictBytes, StrictInt, StrictStr
+from pydantic import BaseModel, Json, StrictBytes, StrictInt, StrictStr
 
 from .cookies import Cookies
 from .url_metadata import URLMetadata
@@ -44,48 +44,104 @@ class HTTPResponse(BaseModel):
             self.status and self.status >= 200 and self.status < 300
         )
     
-    def json(self):
+    def json(
+        self,
+        return_failures: bool = True,
+    ) -> Json | Exception:
 
-        if self.content:
-            return orjson.loads(
-                self.content
-            )
-    
-        return {}
+        try:
+            if self.content:
+                return orjson.loads(
+                    self.content
+                )
         
-    def text(self):
-        return self.content.decode()
+            return {}
+        
+        except Exception as parse_error:
+            
+            if return_failures:
+                return parse_error
+
+            raise parse_error
+        
+    def text(
+        self,
+        encoding: str = 'utf-8',
+        return_failures: bool = True,
+    ) -> str | Exception:
+        try:
+            return self.content.decode(
+                encoding=encoding,
+            )
+        
+        except Exception as parse_error:
+            
+            if return_failures:
+                return parse_error
+
+            raise parse_error
     
     def to_model(
         self,
-        model: Type[T]
-    ) -> T:
-        return model(**orjson.loads(
-            self.content
-        ))
+        model: Type[T],
+        return_failures: bool = True,
+    ) -> T | Exception:
+        try:
+            return model(**orjson.loads(
+                self.content
+            ))
+        
+        except Exception as parse_error:
+            
+            if return_failures:
+                return parse_error
+
+            raise parse_error
+        
+    def unzip(
+        self,
+        return_failures: bool = True,
+    ) -> bytes | Exception:
+        try:
+            return gzip.decompress(self.content)
+        
+        except Exception as parse_error:
+
+            if return_failures:
+                return parse_error
+            
+            raise parse_error
 
     @property
     def data(
         self,
-        model: Optional[Type[T]]=None
+        model: Optional[Type[T]]=None,
+        return_failures: bool = True
     ):
 
         content_type = self.headers.get('content-type')
 
         if model:
-            return self.to_model(model)
+            return self.to_model(
+                model,
+                return_failures=return_failures,
+            )
 
         try:
             match content_type:
 
                 case 'application/json':
-                    return self.json()
+                    return self.json(
+                        return_failures=return_failures,
+                    )
                 
                 case 'text/plain':
                     return self.text()
                 
                 case 'application/gzip':
-                    return gzip.decompress(self.content)
+                    return self.unzip(
+                        return_failures=return_failures,
+                    )
                 
                 case _:
                     return self.content
