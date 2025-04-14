d471ccb70b597671935d2d52a5242655a2845cb5
Ada Lundhe <adalundhe@Adas-MacBook-Pro-2.local>
2025-04-14 10:42:03 -0500

    AL: fix aws module

--- werkflow-aws/werkflow_aws/models/cost_explorer/cost_explorer_response.py
+++ werkflow-aws/werkflow_aws/models/cost_explorer/cost_explorer_response.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel, StrictStr
+from .dimension_value_attribute import DimensionValueAttribute
 from .group_definition import GroupDefinition
 from .result import Result
 
@@ -7,4 +8,4 @@ class CostExplorerResponse(BaseModel):
     NextPageToken: StrictStr
     GroupDefinitions: list[GroupDefinition] | None = None
     ResultsByTime: list[Result]
-    
+    DimensionValueAttributes: list[DimensionValueAttribute]
