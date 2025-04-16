a778a27706daa0916bca26741e30f3eee2fac4fe
Ada Lundhe <adalundhe@Adas-MacBook-Pro-2.local>
2025-04-16 17:05:45 -0500

    AL: add config parsing

--- werkflow-aws/werkflow_aws/models/cost_explorer/result.py
+++ werkflow-aws/werkflow_aws/models/cost_explorer/result.py
@@ -1,11 +1,9 @@
 from pydantic import BaseModel, StrictStr, StrictBool
-from .dimension_value_attribute import DimensionValueAttribute
 from .result_group import ResultGroup
 from .time_period import TimePeriod
 from .total import Total
 
 class Result(BaseModel):
-    DimensionValueAttributes: list[DimensionValueAttribute]
     TimePeriod: TimePeriod
     Total: dict[StrictStr, Total]
     Groups: list[ResultGroup]
