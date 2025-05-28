48020bd244ea2b0cc68ecffe082ee87aee73cc2b
Ada Lundhe <adalundhe@Adas-MacBook-Pro-2.local>
2025-05-28 18:44:03 -0500

    AL: fixed

--- werkflow-aws/werkflow_aws/services/aws_elasticache.py
+++ werkflow-aws/werkflow_aws/services/aws_elasticache.py
@@ -221,6 +221,9 @@ class AWSElastiCache:
     
     async def close(self):
 
+        if self._loop is None:
+            self._loop = asyncio.get_event_loop()
+
         await self._loop.run_in_executor(
             None,
             self._client.close
