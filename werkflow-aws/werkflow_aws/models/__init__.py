e437407b6e4eb78dd8f8cffb4add429a8c649e83
Ada Lundhe <adalundhe@Adas-MacBook-Pro-2.local>
2025-06-10 17:58:39 -0500

    AL: fixing

--- werkflow-aws/werkflow_aws/models/__init__.py
+++ werkflow-aws/werkflow_aws/models/__init__.py
@@ -4,4 +4,5 @@ from .aws_region import AWSRegionMap as AWSRegionMap
 from .aws_region import RegionName as RegionName
 from .aws_services import AWSServices as AWSServices
 from .aws_services import AWSServicesMap as AWSServicesMap
+from .aws_services import MappedServiceNames as MappedServiceNames
 from .aws_services import ServiceName as ServiceName
